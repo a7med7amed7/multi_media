5f5e4b1425b679f2cb78e9a59ee25d26334c6dc7
Muhammed Aly <75846623+mohammedaly915@users.noreply.github.com>
2022-12-21 00:08:18 +0200

    Add files via upload

---
+++ src/components/LinkedList.js
@@ -0,0 +1,53 @@
+class node{
+    constructor(data){
+        this.data=data
+        this.next=null
+        this.pre=null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head=null
+        this.current=null
+    }
+    insert(data){
+        let newNode =new node(data)
+        if (this.head ===null){
+            this.head=newNode
+            this.current=newNode
+        }
+        else{
+            let temp =this.head;
+            while(temp.next !== null){
+                temp=temp.next
+            }
+            temp.next=newNode
+            newNode.pre=temp
+            this.current =newNode
+        }
+        
+    }
+    redo=()=>{
+        const nextData=this.current.next
+        if (nextData){
+            this.current=nextData
+            return nextData.data
+        }
+        else{
+            return null
+        }
+    }
+    undo=()=>{
+        const preData=this.current.pre
+        if (preData){
+            this.current=preData
+            return preData.data
+        }
+        else{
+            return null
+        }
+    }
+}
+ const store =new LinkedList()
+export default store;
