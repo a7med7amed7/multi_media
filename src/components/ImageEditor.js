4a1c905da8fe482f3df0fa0abcfc9cd5c6fe777e
Muhammed Aly <75846623+mohammedaly915@users.noreply.github.com>
2022-12-26 13:19:52 +0200

    Add files via upload

--- src/components/ImageEditor.js
+++ src/components/ImageEditor.js
@@ -1,12 +1,15 @@
 import React,{useState} from 'react'
 import ReactCrop from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
-import store from './LinkedList'
+
 //import "./App1.scss"
 //import Effects from './Efects';
 //import ImagePrev from './ImagePrev';
 
 function ImageEditor() {
+
+    // States
+
     const[state,setState]=useState({
         image:"",
         grayScale:0,
@@ -20,30 +23,6 @@ function ImageEditor() {
     })
     const [details ,setDetails]=useState()
     const [crop, setCrop] = useState()
-    const ShowImage=(e)=>{
-        if(e.target.files.length !== 0) { 
-            const reader =new FileReader()
-            reader.onload =()=>{
-                setState({
-                    ...state,
-                    image:reader.result
-                })
-
-                const stateData ={
-                    image:reader.result,
-                    grayScale:0,
-                    brightness:100,
-                    saturate:100,
-                    contrast:100,
-                    rotate:0,
-                    horizintal:1,
-                    vertical:1
-                }
-                store.insert(stateData)
-            }
-            reader.readAsDataURL(e.target.files[0])
-        }
-    }
     const effects=[
         {name:'grayScale',
         maxValue:'100%'},
@@ -58,23 +37,24 @@ function ImageEditor() {
         name:'grayScale',
         maxValue:'200%'
     })
+
+    // function handel
     const handleChange=(e)=>{
         setState({
             ...state,
             [e.target.name]:e.target.value
         }
-
         )
     }
+
+
     const RotLeft=()=>
     {
         setState({
             ...state,
             rotate:state.rotate+90
         })
-        const stateData =state
-            stateData.rotate=state.rotate+90
-            store.insert(stateData)
+    
     }
     const RotRight=()=>
     {
@@ -82,9 +62,7 @@ function ImageEditor() {
             ...state,
             rotate:state.rotate-90
         })
-        const stateData =state
-        stateData.rotate=state.rotate-90
-        store.insert(stateData)
+    
     }
     const LeftRight=()=>
     {
@@ -92,9 +70,7 @@ function ImageEditor() {
             ...state,
             horizintal:state.horizintal===1?-1:1
         })
-        const stateData =state
-        stateData.horizintal=state.horizintal===1?-1:1
-        store.insert(stateData)
+        
     }
     const topBottom=()=>
     {
@@ -102,17 +78,34 @@ function ImageEditor() {
             ...state,
             vertical:state.vertical===1?-1:1
         })
-        const stateData =state
-        stateData.vertical=state.vertical===1?-1:1
-        store.insert(stateData)
+        
     }
-    const ImageCrop=()=>
+
+    const ShowImage=(e)=>{
+        if(e.target.files.length !== 0) { 
+            const reader =new FileReader()
+            reader.onload =()=>{
+                setState({
+                    ...state,
+                    image:reader.result
+                })
+
+            }
+            reader.readAsDataURL(e.target.files[0])
+        }
+    }
+
+    
+    const ImageCrop=(e)=>
     {
+        
         const canava=document.createElement("canvas");
         const scaleX=details.naturalWidth/details.width;
         const scaleY=details.naturalHeight/details.height;
         canava.width=crop.width
         canava.height=crop.height
+        
+        
         const ctx =canava.getContext("2d")
         ctx.drawImage(
             details,
@@ -124,40 +117,30 @@ function ImageEditor() {
             0,
             crop.width,crop.height
         )
+        
+        
         const base64Url = canava.toDataURL("image.jpg")
         setState({
             ...state,
             image:base64Url
         })
-
+        
     }
-    const redo=()=>{
-        const data = store.redo()
-        if (data){
-            setState(data)
-        }
-    }
-    const undo=()=>{
-        const data = store.undo()
-        if (data){
-            setState(data)
-        }
-    }
+    
+        
     const Save=()=>{
-        const canava=document.createElement("canvas");
-        //const scaleX=details.naturalWidth/details.width;
-        //const scaleY=details.naturalHeight/details.height;
+        const canava=document.createElement("canvas");       
         canava.width=details.naturalWidth
         canava.height=details.naturalHeight
         const ctx =canava.getContext("2d")
         ctx.filter=`brightness(${state.brightness}%) saturate(${state.saturate}%) contrast(${state.contrast}%) grayscale(${state.grayScale}%)`
         ctx.transform=` scale(${state.vertical},${state.horizintal })`
         ctx.translate(canava.width/2 ,canava.height/2)
-        ctx.rotate(state.rotate * Math.PI/100)
+        ctx.rotate(state.rotate * Math.PI/180)
         ctx.drawImage(
             details,
-            canava.width/2,
-            canava.height/2,
+            -canava.width/2,
+            -canava.height/2,
             canava.width,
             canava.height
         )
@@ -180,30 +163,32 @@ function ImageEditor() {
                 vertical:1
             
         })
-        store.rest()
+        
     }
+    
  return (
     <>
         <div className='editor'>
             <div className='card'>
-                <div className='card-head'>
-                    <h1>foto Shob</h1>
-                </div>
 
                 <div className='card-body'>
                         <div className='image-sec'>
                             <div className='Img'>
                             {
                                 state.image?<ReactCrop crop={crop} onChange={c => setCrop(c)}>
-                                <img onLoad={(e)=>setDetails(e.currentTarget)} style={{filter:`brightness(${state.brightness}%) saturate(${state.saturate}%) contrast(${state.contrast}%) grayscale(${state.grayScale}%)`, transform:`rotate(${state.rotate}deg) scale(${state.vertical},${state.horizintal})`}} src={state.image} alt="Loading..."/></ReactCrop>:<label className='Choose' htmlFor="choose">Choose Image</label>
+                                <img onLoad={(e)=>{setDetails(e.currentTarget)
+                                                    
+                                }} 
+                                style={{filter:`brightness(${state.brightness}%) saturate(${state.saturate}%) contrast(${state.contrast}%) grayscale(${state.grayScale}%)`, 
+                                        transform:`rotate(${state.rotate}deg) scale(${state.vertical},${state.horizintal})`}} 
+                                        src={state.image} alt="Loading..."/></ReactCrop>
+                                        :<label className='Choose' htmlFor="choose">Choose Image</label>
                             }
                             </div>
                         
                             <div className='select-img'>
-                                <button onClick={undo} className='undo btn'>Undo</button>
-                                <button onClick={redo} className='redo btn'>Redo</button>
                                 {
-                                    crop&&<button onClick={ImageCrop} className='crop btn'>Crop</button>
+                                    crop&&<button onClick={()=>ImageCrop()} className='crop btn'>Crop</button>
                                 }
                                 <label className='Choose btn' htmlFor="choose">Choose Image</label>
                                 <input type="file" onChange={ShowImage} id="choose" hidden/>
@@ -219,12 +204,11 @@ function ImageEditor() {
                                                     setFilter(effect)
                                                     console.log(state[filter.name])
                                                     }
-
                                                 } effect={effect.name} key={i}>{effect.name} </button>)
                                                 
                                             }
                                             <div>
-                                            <input   name={filter.name} onChange={handleChange}  value={state[filter.name]}  max={filter.maxValue} type="range" className='range' /><span>{Math.floor(state[filter.name]/2)}</span>
+                                            <input   name={filter.name} onChange={handleChange}  value={state[filter.name]}  max="200" type="range" className='range' /><span> {state[filter.name]}%</span>
                                             </div>
                                         </div>
                                 </div>
